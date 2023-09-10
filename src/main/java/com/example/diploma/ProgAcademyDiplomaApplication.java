33706c8c3d439dbc042f4db84a624dc6437e1969
-=And=- <Pozand47@gmail.com>
2023-09-10 17:44:02 +0200

    1

---
+++ src/main/java/com/example/diploma/ProgAcademyDiplomaApplication.java
@@ -0,0 +1,13 @@
+package com.example.diploma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgAcademyDiplomaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProgAcademyDiplomaApplication.class, args);
+    }
+
+}
